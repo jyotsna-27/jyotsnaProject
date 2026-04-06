197772392f976f3359e270d32cbd3917fbbe3dbf
jyotsna <your - jyotsnamudgal@gmail.com>
2026-04-06 09:09:38 +0530

    First java program

--- Demo.java
+++ Demo.java
@@ -1,9 +1,21 @@
-class Demo {
- 
-    public static void main(String []args){
-        System.out.println("Amit Diwan");
+import java.util.Scanner;
 
+public class Demo{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+
+        sc.close();
     }
-    
-    
 }
+    
+
